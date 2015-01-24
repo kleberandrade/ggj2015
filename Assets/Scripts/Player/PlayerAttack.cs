4649cc273b4d7283e5076e7224ea544fcd96fcb2
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-01-24 01:03:12 -0200

    adding player attack script

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour 
+{
+    public float timeBetweenAttack = 0.2f;
+
+    private AudioSource attackClip;
+    private Animator anim;
+    private float timer;
+
+	void Awake () 
+    {
+        attackClip = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () 
+    {
+        timer += Time.deltaTime;
+	    if (Input.GetButton("Fire1") && timer >= timeBetweenAttack)
+            Attack();
+	}
+
+    void Attack()
+    {
+        timer = 0.0f;
+        attackClip.Play();
+        anim.SetTrigger("Attack");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
