7d3ed56e6f6502d3804d3fe9ebde8d93dfc398e0
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-01-24 01:03:41 -0200

    adding player health

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public AudioClip deathClip;
+
+    private PlayerAttack playerAttackScrip;
+    private PlayerMovement playerMovementScrip;
+    private AudioSource playerAudio;
+    private Animator anim;
+
+	void Awake () 
+    {
+        playerAttackScrip = GetComponent<PlayerAttack>();
+        playerMovementScrip = GetComponent<PlayerMovement>();
+        playerAudio = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+	}
+	
+	public void Kill () 
+    {
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        anim.SetTrigger("Die");
+        playerAttackScrip.enabled = false;
+        playerMovementScrip.enabled = false;
+    }
+}
