6d41019679b87c67403819b9371e0e92e61d0ee1
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-01-29 00:17:08 -0200

    Separado a parte de sons do jogador dos outros scripts (script PlayerSound criado)

---
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSound : MonoBehaviour 
+{
+    public AudioClip attackClip;
+    public AudioClip deathClip;
+
+    private AudioSource source;
+
+    void Awake () 
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayAttackClip()
+    {
+        source.PlayOneShot(attackClip, 1.0f);
+    }
+
+    public void PlayDeathClip()
+    {
+        source.PlayOneShot(deathClip, 1.0f);
+    }
+}
