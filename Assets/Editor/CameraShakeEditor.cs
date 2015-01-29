4840e531d2433f83acad8854eec184091ac34bd3
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-01-29 12:26:52 -0200

    Melhorias no script de CameraShake e modificação em seu Inspector (CameraShakeEditor) para testes.

---
+++ Assets/Editor/CameraShakeEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraShake))]
+public class CameraShakeEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        CameraShake cameraShake = (CameraShake)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Shake"))
+            cameraShake.Shake();       
+    }
+}
--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -1,41 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[AddComponentMenu("Scripts/Camera/CameraShake")]
 public class CameraShake : MonoBehaviour
 {
-    private Vector3 originalPos;
+    /// <summary>
+    /// Referência para o transfom da câmera
+    /// </summary>
     private Transform camTransform;
+
+    /// <summary>
+    /// Valor inicial do shake
+    /// </summary>
     private float shake = 0f;
 
+    /// <summary>
+    /// Total do shake
+    /// </summary>
     public float shakeAmount = 0.08f;
+
+    /// <summary>
+    /// Fator de decremento do shake
+    /// </summary>
     public float decreaseFactor = 2.0f;
 
     void Awake()
     {
-        if (camTransform == null)
-            camTransform = GetComponent(typeof(Transform)) as Transform;
-    }
-
-    void OnEnable()
-    {
-        originalPos = camTransform.localPosition;
+        camTransform = GetComponent<Transform>();
     }
 
     void Update()
     {
-        if (shake > 0)
+        if (shake > 0.0f)
         {
-            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+            camTransform.localPosition += Random.insideUnitSphere * shakeAmount;
             shake -= Time.deltaTime * decreaseFactor;
         }
         else
         {
-            shake = 0f;
-            camTransform.localPosition = originalPos;
+            shake = 0.0f;
         }
     }
 
-    void Shake()
+    public void Shake()
     {
         shake = 1.0f;
     }
