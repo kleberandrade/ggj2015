a7a9a3c98fb6312f085748ab2c4e03fa6724d411
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-02-05 22:18:21 -0200

    Adicionado classe para pooler de objetos

---
+++ Assets/Scripts/Helper/ObjectPooler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public int pooledAmount = 3;
+    public bool willGrow = true;
+    private List<GameObject> pooledObjects;
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject go = (GameObject)Instantiate(objectPrefab);
+            go.SetActive(false);
+            pooledObjects.Add(go);
+        }
+    }
+
+    public GameObject NextObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+                return pooledObjects[i];
+        }
+
+        if (willGrow)
+        {
+            GameObject go = (GameObject)Instantiate(objectPrefab);
+            pooledObjects.Add(go);
+            return go;
+        }
+
+        return null;
+    }
+}
