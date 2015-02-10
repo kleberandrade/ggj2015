4ad092e1b6ca1a329c3bf338061cd9f5e0c9ff39
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-02-10 01:15:37 -0200

    Adicionado indicação de qual jogador control qual personagem

---
+++ Assets/Scripts/Camera/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LookAtCamera : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    public Text textName;
+    private Camera sourceCamera;
+    
+    void Start()
+    {
+        sourceCamera = Camera.main;
+        textName.text = string.Format("Player {0}", playerInput.GetControlNumber);
+    }
+
+    void Update()
+    {
+        if (sourceCamera != null)
+            transform.rotation = sourceCamera.transform.rotation;
+    }
+}
